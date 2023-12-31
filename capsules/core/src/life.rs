fb41de5c4c73155059bbbd0e6bc525e9e3cfcdb3
Jason <jasonlu2025@u.northwestern.edu>
2023-12-31 15:36:09 -0600

    Replaced LED structs with simplified structs

--- capsules/core/src/life.rs
+++ capsules/core/src/life.rs
@@ -61,53 +61,39 @@ use kernel::{ErrorCode, ProcessId};
 use crate::driver;
 pub const DRIVER_NUM: usize = driver::NUM::Life as usize;
 
-impl<L: led::Led, const NUM_LEDS: usize> SyscallDriver for LedDriver<'_, L, NUM_LEDS> {
+/// Implements a basic SyscallDriver without any specific device management.
+pub struct LifeDriver;
+
+impl BasicDriver {
+    pub fn new() -> Self {
+        // Initialization logic can be added if needed in the future.
+        Self
+    }
+}
+
+impl SyscallDriver for BasicDriver {
     /// Control the LEDs.
     ///
     /// ### `command_num`
     ///
-    /// - `0`: Returns the number of LEDs on the board. This will always be 0 or
-    ///        greater, and therefore also allows for checking for this driver.
-    /// - `1`: Turn the LED at index specified by `data` on. Returns `INVAL` if
-    ///        the LED index is not valid.
-    /// - `2`: Turn the LED at index specified by `data` off. Returns `INVAL`
-    ///        if the LED index is not valid.
-    /// - `3`: Toggle the LED at index specified by `data` on or off. Returns
-    ///        `INVAL` if the LED index is not valid.
+    /// - `0`: Returns the meaning of life (42) as a u32. This is a simple
+    ///        example of a command that returns data.
+    /// - `1`: Returns a failure code if the data is not 42. This is a simple
+    ///        example of a command that returns a failure code.
+    ///
     fn command(&self, command_num: usize, data: usize, _: usize, _: ProcessId) -> CommandReturn {
         match command_num {
             // get number of LEDs
             // TODO(Tock 3.0): TRD104 specifies that Command 0 should return Success, not SuccessU32,
             // but this driver is unchanged since it has been stabilized. It will be brought into
             // compliance as part of the next major release of Tock. See #3375.
-            0 => CommandReturn::success_u32(NUM_LEDS as u32),
+            0 => CommandReturn::success_u32(42 as u32),
 
             // on
             1 => {
-                if data >= NUM_LEDS {
+                if data != 42 {
                     CommandReturn::failure(ErrorCode::INVAL) /* led out of range */
                 } else {
-                    self.leds[data].on();
-                    CommandReturn::success()
-                }
-            }
-
-            // off
-            2 => {
-                if data >= NUM_LEDS {
-                    CommandReturn::failure(ErrorCode::INVAL) /* led out of range */
-                } else {
-                    self.leds[data].off();
-                    CommandReturn::success()
-                }
-            }
-
-            // toggle
-            3 => {
-                if data >= NUM_LEDS {
-                    CommandReturn::failure(ErrorCode::INVAL) /* led out of range */
-                } else {
-                    self.leds[data].toggle();
                     CommandReturn::success()
                 }
             }
